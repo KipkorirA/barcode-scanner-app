b0e91314e644508954c7788827fc7036902856de
KipkorirA <aron.kipkorir@student.moringaschool.com>
2025-01-15 08:40:02 +0300

    Done

--- src/components/BarcodeScanner/BarcodeScanner.jsx
+++ src/components/BarcodeScanner/BarcodeScanner.jsx
@@ -118,18 +118,10 @@ const BarcodeScanner = () => {
           aspectRatio: 1.0,
           formatsToSupport: [
             Html5QrcodeSupportedFormats.EAN_13,
-            Html5QrcodeSupportedFormats.EAN_8,
-            Html5QrcodeSupportedFormats.UPC_A,
-            Html5QrcodeSupportedFormats.UPC_E,
             Html5QrcodeSupportedFormats.CODE_128,
             Html5QrcodeSupportedFormats.CODE_39,
             Html5QrcodeSupportedFormats.CODE_93,
-            Html5QrcodeSupportedFormats.CODABAR,
             Html5QrcodeSupportedFormats.ITF,
-            Html5QrcodeSupportedFormats.QR_CODE,
-            Html5QrcodeSupportedFormats.DATA_MATRIX,
-            Html5QrcodeSupportedFormats.AZTEC,
-            Html5QrcodeSupportedFormats.PDF_417
           ],
           showTorchButtonIfSupported: true,
           experimentalFeatures: {
